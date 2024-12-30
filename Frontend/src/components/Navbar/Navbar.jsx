e46bd7029cc6bb058524bed1e7e744b4baa401f3
Bibek <137331749+immbibek@users.noreply.github.com>
2024-12-30 20:43:36 +0530

    Navbar creation

---
+++ Frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+     <nav className=''>
+       <ul className='flex text-white items-center justify-around bg-zinc-800'>
+        <li>Logo</li>
+        <li>Home</li>
+        <li>Projects</li>
+        <li>Search</li>
+        <li>add projects</li>
+        <li>Profile</li>
+       </ul>
+    
+     </nav>
+    </div>
+  )
+}
+
+export default Navbar
