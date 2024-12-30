e8fd7e8b2de21627e7da99fbae543307a3836c9b
kumar chaudhary <chaudharykumar228@gmail.com>
2024-12-30 20:54:14 +0530

    user routes, user controller, post routes and post controller

--- Backend/controller/userController.js
+++ Backend/controller/userController.js
@@ -39,29 +39,3 @@ export const createUser = async (req, resp) => {
   });
 };
 
-// export const createUser = async (req, res) => {
-//   const { name, email, password } = req.body;
-
-//   const findUser = await prisma.user.findUnique({
-//     where: {
-//       email: email,
-//     },
-//   });
-
-//   if (findUser) {
-//     return res.json({
-//       status: 400,
-//       message: "Email Already Taken . please another email.",
-//     });
-//   }
-
-//   const newUser = await prisma.user.create({
-//     data: {
-//       name: name,
-//       email: email,
-//       password: password,
-//     },
-//   });
-
-//   return res.json({ status: 200, data: newUser, msg: "User created." });
-// };
--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,17 +1,3 @@
-// import { Router } from "express";
-// import { createUser} 
-// from "../controller/userController.js";
-
-// // const userRoutes = Router();
-// // userRoutes.post("/",createUser);
-
-// // export default userRoutes;
-
-// const router = Router();
-// router.post("/", createUser);
-// export default router;
-
-
 import { Router } from "express";
 import {
   createUser,
