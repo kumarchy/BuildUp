2853530ce547a5f3187651675c31cd786af667d3
kumar chaudhary <chaudharykumar228@gmail.com>
2024-12-31 23:42:29 +0530

    created form page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import Form from './components/Form/Form'
 const App = () => {
   return (
     <div>
      <Navbar/>
+     <Form/>
     </div>
   )
 }
