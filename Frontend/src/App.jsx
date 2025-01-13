613c9cee54f7fbc04a9a9adeb363b8de5694d718
Bibek <137331749+immbibek@users.noreply.github.com>
2025-01-13 23:39:14 +0530

    structure of Home page done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import Content from './components/Homecomp/Content'
 const App = () => {
   return (
     <div>
      <Navbar/>
+     <Content/>
+     
     </div>
   )
 }
