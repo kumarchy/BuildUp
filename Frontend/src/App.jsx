b9b5f0b23e8f1107128a9aba7689243a8258e396
kumar chaudhary <chaudharykumar228@gmail.com>
2025-01-18 20:54:33 +0530

    added other component to app component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
-const App = () => {
-  return (
-    <div>
-     <Navbar/>
-    </div>
-  )
-}
+import React, { useState } from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Form from "./components/Form/Form";
+import Signup from "./components/Signup/Signup";
 
-export default App
+const App = () => {
+  const [showSignup, setShowSignup] = useState(false);
+
+  return (
+   <div>
+    <Navbar setShowSignup={setShowSignup}/>
+    {showSignup && <Signup/>}
+    <Form />
+   </div>
+  );
+};
+
+export default App;
