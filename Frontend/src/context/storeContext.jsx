2be7bc9ee053a8107ad7269ca2719283b9b71c68
kumar chaudhary <chaudharykumar228@gmail.com>
2025-01-21 22:12:13 +0530

    integrated frontend with backend

---
+++ Frontend/src/context/storeContext.jsx
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import { createContext, useEffect, useState } from "react";
+
+export const StoreContext = createContext(null);
+
+
+const StoreContextProvider = (props) => {
+  const [showPersonalPost, setShowPersonalPost] = useState("");
+
+  const url="http://localhost:3000";
+
+  const showProjects = async() =>{
+  try{
+    const response = await axios.get(`${url}/api/post`)
+    if(response.data.success){
+      setShowPersonalPost(response.data.data);
+    }
+  }catch(error){
+    console.log(error);
+  }
+  }
+
+  const contextValue ={
+  showPersonalPost,
+  showProjects
+  }
+
+  return (
+    <StoreContext.Provider value={contextValue}>
+    {props.children}
+    </StoreContext.Provider>
+  )
+}
+
+export default StoreContextProvider;
