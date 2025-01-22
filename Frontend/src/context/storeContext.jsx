af29197dcf403183a5c3bf28ae4523cf2bf94e08
kumar chaudhary <chaudharykumar228@gmail.com>
2025-01-22 22:19:24 +0530

    added display all post code in context

--- Frontend/src/context/storeContext.jsx
+++ Frontend/src/context/storeContext.jsx
@@ -1,48 +1,62 @@
-import axios from 'axios';
+import axios from "axios";
 import { createContext, useEffect, useState } from "react";
 
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [showPersonalPost, setShowPersonalPost] = useState("");
+  const [showProjectDetail, setShowProjectDetail] = useState("");
 
-  const url="http://localhost:3000";
-  
+  const url = "http://localhost:3000";
+
   // display user personal projects
-  const showProjects = async() =>{
-  try{
-    const response = await axios.get(`${url}/api/post`)
-    if(response.data.success){
-      setShowPersonalPost(response.data.data);
-    }
-  }catch(error){
-    console.log(error);
-  }
-  }
-
-  // delete project
-  const deleteProjects = async({postId}) =>{
-    try{
-     const response = await axios.delete(`${url}/api/post/${postId}`);
-     if(response.data.success){
-      console.log("post deleted successfully");
-     }
-    }catch(error){
+  const showProjects = async () => {
+    try {
+      const response = await axios.get(`${url}/api/post`);
+      if (response.data.success) {
+        setShowPersonalPost(response.data.data);
+      }
+    } catch (error) {
       console.log(error);
     }
-  }
+  };
 
-  const contextValue ={
-  showPersonalPost,
-  showProjects,
-  deleteProjects
-  }
+  const projectDetails = async () => {
+    try {
+      const response = await axios.get(`${url}/api/post`);
+      if (response.data.success) {
+        setShowProjectDetail(response.data.data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // delete project
+  const deleteProjects = async ({ postId }) => {
+    try {
+      const response = await axios.delete(`${url}/api/post/${postId}`);
+      if (response.data.success) {
+        console.log("post deleted successfully");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const contextValue = {
+    showPersonalPost,
+    showProjects,
+    deleteProjects,
+    showProjectDetail,
+    projectDetails,
+  };
 
   return (
     <StoreContext.Provider value={contextValue}>
-    {props.children}
+      {props.children}
     </StoreContext.Provider>
-  )
-}
+  );
+};
 
 export default StoreContextProvider;
