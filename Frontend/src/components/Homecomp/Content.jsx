1c7fbf4455299b3a5b1a866f344a4c3778229dcc
kumar chaudhary <chaudharykumar228@gmail.com>
2025-01-30 22:19:45 +0530

    updated

--- Frontend/src/components/Homecomp/Content.jsx
+++ Frontend/src/components/Homecomp/Content.jsx
@@ -14,7 +14,7 @@ const Content = () => {
   const navigate = useNavigate();
   const handleUserProjectsClick = () => {
     if (user && user.id) {
-      navigate(`/${user.id}/`);
+      navigate(`/${user.id}/personalProjects`);
     } else {
       console.log("User ID not found");
     }
