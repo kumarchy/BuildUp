d68ff747b92d0fee67276206a28dde31eafaf1be
kumar chaudhary <chaudharykumar228@gmail.com>
2025-02-01 11:07:49 +0530

    updated code

--- Frontend/src/pages/ProjectDetail/ProjectDetail.jsx
+++ Frontend/src/pages/ProjectDetail/ProjectDetail.jsx
@@ -1,21 +1,23 @@
 import React, { useContext, useEffect, useState } from "react";
-import { useNavigate, useParams} from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { StoreContext } from "../../context/storeContext";
 
 const ProjectDetail = () => {
   const { projectId } = useParams();
-  const {showAllPost} = useContext(StoreContext);
+  const { showAllPost } = useContext(StoreContext);
   const [project, setProject] = useState(null);
 
-  useEffect(()=>{
-    if(showAllPost){
-      const selectedProject  = showAllPost.find((p)=> p.id==projectId);
-      setProject(selectedProject)
+  useEffect(() => {
+    if (showAllPost) {
+      const selectedProject = showAllPost.find(
+        (p) => p.id == Number(projectId)
+      );
+      setProject(selectedProject);
     }
-  },[showAllPost,projectId]);
+  }, [showAllPost, projectId]);
 
-  if(!project){
-    return <div className="text-white">Loading...</div>
+  if (!project) {
+    return <div className="text-white">Loading...</div>;
   }
 
   // const user = JSON.parse(localStorage.getItem("user"));
@@ -32,9 +34,7 @@ const ProjectDetail = () => {
     <div className="relative flex flex-col items-center dark:bg-zinc-900 text-white">
       <div className="md:w-[70%] w-[95%]">
         <div className="flex items-center justify-center ">
-          <div
-            className="font-bold h-10 w-10 rounded-full bg-blue-500 flex justify-center items-center absolute text-2xl cursor-pointer lg:left-32 md:left-10 sm:top-7 left-3 "
-          >
+          <div className="font-bold h-10 w-10 rounded-full bg-blue-500 flex justify-center items-center absolute text-2xl cursor-pointer lg:left-32 md:left-10 sm:top-7 left-3 ">
             k
           </div>
           <h1 className="sm:text-5xl text-2xl sm:font-bold font-bold mb-3 mt-5">
@@ -50,14 +50,12 @@ const ProjectDetail = () => {
         </div>
         <div className="mt-4">
           <h1 className="text-lg font-semibold">Project Description</h1>
-          <p>
-            {project.description}
-          </p>
+          <p>{project.description}</p>
           <div className="mt-4">
             <h1 className="text-lg font-semibold">Tech stack</h1>
             <ul className="sm:gap-8 gap-5 list-disc ml-6">
-              {project.techStack.map((stack)=>{
-                <li key={stack.id}>{stack}</li>
+              {project.techStack?.map((stack, index) => {
+                <li key={index}>{stack}</li>;
               })}
             </ul>
           </div>
@@ -72,29 +70,33 @@ const ProjectDetail = () => {
             </ul>
           </div>
 
-          <div className="mt-4">
-            <h1 className="text-lg font-semibold">Visit Website</h1>
-            <a
-              href={`${project.deployedLink}`}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="underline text-blue-600"
-            >
-              {project.deployedLink}
-            </a>
-          </div>
+          {project.deployedLink && (
+            <div className="mt-4">
+              <h1 className="text-lg font-semibold">Visit Website</h1>
+              <a
+                href={project.deployedLink}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="underline text-blue-600"
+              >
+                {project.deployedLink}
+              </a>
+            </div>
+          )}
 
-          <div className="mt-4 pb-16">
-            <h1 className="text-lg font-semibold">Visit Github Repository</h1>
-            <a
-              href={`${project.githubLink}`}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="underline text-blue-600"
-            >
-              {project.githubLink}
-            </a>
-          </div>
+          {project.githubLink && (
+            <div className="mt-4 pb-16">
+              <h1 className="text-lg font-semibold">Visit GitHub Repository</h1>
+              <a
+                href={project.githubLink}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="underline text-blue-600"
+              >
+                {project.githubLink}
+              </a>
+            </div>
+          )}
         </div>
       </div>
     </div>
