485e5d062ca53971f6210b1624b53e8097b02109
kumar chaudhary <chaudharykumar228@gmail.com>
2025-02-19 00:52:33 +0530

    updated code

--- Backend/controller/commentController.js
+++ Backend/controller/commentController.js
@@ -1,43 +1,65 @@
+import e from "express";
 import prisma from "../db/db.config.js";
 
-//create comment
+// create a comment
 export const createComment = async (req, resp) => {
-  const { post_id, user_id, comment } = req.body;
+  const { user_id, post_id, comment } = req.body;
+
+  await prisma.post.update({
+    where:{
+      id:Number(post_id)
+    },
+    data:{
+      comment_count:{
+        increment:1
+      }
+    }
+  })
+  
+  const newComment = await prisma.comment.create({
+    data: {
+      user_id: Number(user_id),
+      post_id: Number(post_id),
+      comment: comment,
+    },
+  });
+
+  return resp.json({
+    status: 200,
+    data: newComment,
+    message: "comment created Successfully",
+  });
+};
+
+// // fetch all comments
+export const fetchAllComment = async(req, resp)=>{
+  const comment = await prisma.post.findMany();
+  return resp.json({status:200, data:comment}
+  )
+}
+
+// show perticular post comment
+export const showPostComment = async (req, resp) => {
+  const postId = Number(req.params.post_id);
 
   try {
-    await prisma.comment.create({
-      data: {
-        post_id: Number(post_id),
-        user_id: Number(user_id),
-        comment: comment,
+    const comments = await prisma.comment.findMany({
+      where: {
+        post_id: postId,
+      },
+      include: {
+        user: {
+          select: {
+            name: true,
+            email: true,
+          },
+        },
       },
     });
 
-    resp.json({
-      status: 200,
-      success: true,
-      message: "Comment created successfully",
-    });
+    return resp.json({ status: 200, data: comments });
   } catch (error) {
     console.error(error);
-    resp.json({
-      status: 400,
-      message: "An error occured while creating comment",
-    });
-  }
-};
-
-// showAll Comment
-export const fetchAllComment = async (req, resp) => {
-  const comments = await prisma.comment.findMany();
-
-  try {
-    return resp.json({ status: 200, success: true, data: comments });
-  } catch (error) {
-    console.error(error);
-    return resp.json({
-      status: 400,
-      message: "An error occurred while fetching comments",
-    });
+    return resp.status(500).json({ message: "Something went wrong", error });
   }
 };
--- Backend/server.js
+++ Backend/server.js
@@ -21,7 +21,8 @@ app.use(
 );
 
 app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
 
 app.use(router);
 
