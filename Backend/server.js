dadd0ef972737b049acf5cf64c8a3a169669c4ca
kumar chaudhary <chaudharykumar228@gmail.com>
2025-02-18 23:59:42 +0530

    updated code

--- Backend/server.js
+++ Backend/server.js
@@ -20,8 +20,8 @@ app.use(
   })
 );
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 app.use(router);
 
